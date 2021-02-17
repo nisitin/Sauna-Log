d4d5cc0a136e05884d9b33906bdaad78e79e49b4
nisitin <seiya110307@icloud.com>
2021-02-17 11:48:51 +0900

    フォーム作成しました

--- client/src/Components/auth/Login.tsx
+++ client/src/Components/auth/Login.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handelSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
     return (
         <>
             <h1>ログイン</h1>
+            <form>
+        
+            <div className="form-group-email">
+                <input
+                  type="email"
+                  placeholder="aaa@sample.com"
+                  className="form-control"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
+            </div>
+            <div className="form-group-email">
+                <input
+                  type="text"
+                  placeholder="xxxxxxxxx"
+                  className="form-control"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
+            </div>
+
+             <button>送信</button>
+             </form>
         </>
     )
 }
--- client/src/Components/auth/Register.tsx
+++ client/src/Components/auth/Register.tsx
@@ -1,14 +1,55 @@
 import React,{useState} from "react";
 
+
+// interface Props {
+//     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+// }
+
 const Register = () => {
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+       e.preventDefault()
+       console.table({name, email, password})
+    }
 
     return (
         <>
-            <h1>登録</h1>
+        <h1>登録</h1>
+        <form>
+            <div className="form-group-email">
+                <input
+                  type="text"
+                  placeholder="佐藤　太郎"
+                  className="form-control"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  />
+            </div>
+            <div className="form-group-email">
+                <input
+                  type="email"
+                  placeholder="aaa@sample.com"
+                  className="form-control"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
+            </div>
+            <div className="form-group-email">
+                <input
+                  type="text"
+                  placeholder="xxxxxxxxx"
+                  className="form-control"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
+            </div>
+
+             <button className="btn-submit">送信</button>
+        </form>
         </>
     )
 }
