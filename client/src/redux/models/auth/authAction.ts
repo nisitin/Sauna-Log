15ea9fa330276d654f53bde684feab1d0a4e5e4e
nisitin <seiya110307@icloud.com>
2021-02-28 22:27:57 +0900

    actionの作成

--- client/src/redux/models/auth/authAction.ts
+++ client/src/redux/models/auth/authAction.ts
@@ -1,13 +1,1 @@
-import { Dispatch } from "redux";
 import axios from "axios";
-import {
-  AuthAction,
-  RegisterUser,
-  loginInUser,
-  currentUser,
-} from "../auth/authTypes";
-import { setAuthToken } from "../auth/auth.util";
-import { History } from "history";
-import { AUTH_ERROR } from "../../constants";
-
-// export const registerUser = (userData: )
