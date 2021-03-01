2bc1363b4c98c356716b9669590c1c055c1b9a5d
nisitin <seiya110307@icloud.com>
2021-03-01 17:46:15 +0900

    home画面の軽い調整

--- client/src/Page/Home.tsx
+++ client/src/Page/Home.tsx
@@ -2,11 +2,23 @@ import React from "react";
 
 const Home = () => {
     return (
-        <>
-            <div className="top">
+        <div className="top">
+            <div　className="container">
              <h1>Home</h1>
+             <div className="logo">
+                 {/* <img src="./images/Super Sogbu.png"/> */}
+             </div>
             </div>
-        </>
+            <div>
+              <input />
+            </div>
+            <div className="sauna-pic">
+                <p>サウナの効能って何？</p>
+                <div className="sauna-pic-image">
+
+                </div>
+            </div>
+        </div>
     )
 }
 
--- client/src/redux/models/auth/authAction.ts
+++ client/src/redux/models/auth/authAction.ts
@@ -1,1 +1,28 @@
 import axios from "axios";
+import { LOGOUT } from "../../constants";
+import { RegisterUser } from "./authTypes";
+
+export const loadUser = () => (dispatch: Function, getState: Function) => {
+  dispatch({ type: "aaa" });
+
+  axios.get("/api/auth/user");
+};
+
+export const Register = ({
+  name,
+  email,
+  password,
+  password2,
+}: RegisterUser) => (dispatch: Function) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
+
+export const Logout = () => {
+  return {
+    type: LOGOUT,
+  };
+};
