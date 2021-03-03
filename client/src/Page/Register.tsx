0d17fa40f571c6699fa378720e54dfe5693283fb
nisitin <seiya110307@icloud.com>
2021-03-03 20:58:23 +0900

    明日reduxの調整します

--- client/src/Page/Register.tsx
+++ client/src/Page/Register.tsx
@@ -2,36 +2,45 @@ import React,{useState} from "react";
 import RegisterForm from "../Components/Form/RegisterForm";
 import {useDispatch, useSelector} from "react-redux";
 import { RootState } from "../redux/models/rootreducer";
+import {Register} from "../redux/models/auth/authAction";
+import {RegisterUser} from "../redux/models/auth/authTypes";
 import { Redirect } from "react-router-dom";
 
-interface RegisterValues {
-    name: string
-    email: string,
-    password: string,
-    password2: string
-}
 
 const Register =  () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [password2, setPassword2] = useState('')
-
 
     const dispatch = useDispatch();
     const {isAuthenticated, isLoading} = useSelector((state: RootState) => state.auth);
+
 
     // ここでサインインされていなかったらホームページに戻る処理
     if (isAuthenticated){
         return <Redirect to="/"/>
     }
 
+    const FormSubmit = (formSubmit: RegisterUser) => {
+        const {name, email, password, password2} = formSubmit;
+
+        dispatch(Register({name, email, password, password2}))
+    }
+
     return (
-        <div>
-            <h1>登録</h1>
-            <RegisterForm 
-                
-            />
+        <div className="register-container"> 
+            <form　className="register-form">
+                <div className="name">
+                    <label>お名前</label>
+                    <input name="name" placeholder="佐藤太郎" />
+                </div>
+                <div className="email">
+                    <label>メールアドレス</label>
+                    <input name="email" placeholder="aaaa@sample.com"  />
+                </div>
+                <div className="password">
+                    <label>パスワード</label>
+                    <input name="password" placeholder="xxxxxxxx" />
+                </div>
+                <input type="submit"/>
+            </form>
         </div>
     )
 }
